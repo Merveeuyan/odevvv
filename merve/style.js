dbee62179d756475961821291205ec616b2d3eaa
Merveeuyan <146030686+Merveeuyan@users.noreply.github.com>
2024-07-03 15:21:59 -0400

    Add files via upload

---
+++ merve/style.js
@@ -0,0 +1,11 @@
+let a = (2, 3, 5);
+console.log(a);
+
+let test = 8 > 5 && 2;
+console.log(test);
+
+for (let a = 1; a < 7; a+= 2 ) {
+    console.log('test')
+}
+
+const element = <h1 style={ backgroundColor: "blue"  }>Hi</h1>;
